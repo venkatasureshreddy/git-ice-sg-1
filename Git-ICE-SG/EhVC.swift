eed03b984f666ef43593b3f47c69c086d59911aa
Student <s525715@ch140003m.nwmissouri.edu>
2016-11-14 13:38:28 -0600

    Added EhVC,BeeVC and SeeVC

--- Git-ICE-SG/EhVC.swift
+++ Git-ICE-SG/EhVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  Git-ICE-SG
 //
 //  Created by Ganapam,Venkata Suresh Reddy on 11/14/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class EhVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Git-ICE-SG/SeaVC.swift
@@ -0,0 +1,35 @@
+//
+//  SeaVC.swift
+//  Git-ICE-SG
+//
+//  Created by Ganapam,Venkata Suresh Reddy on 11/14/16.
+//  Copyright © 2016 Ganapam,Venkata Suresh Reddy. All rights reserved.
+//
+
+import UIKit
+
+class SeaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
